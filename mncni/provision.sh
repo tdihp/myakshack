5cca31afb50573d62e3fe650fd3b210cc4bfaaac
Ping He <tdihp@hotmail.com>
2023-03-23 18:26:23 +0800

    mncni: update provision.sh

--- mncni/provision.sh
+++ mncni/provision.sh
@@ -1,17 +1,10 @@
 set -xe
 set -o pipefail
 
-RG=mccni
+RG=mncni
 REGION=southeastasia
-AKSNAME=mccni
+AKSNAME=mncni
 
 az group create -n $RG -l $REGION
-az aks create -k 1.25.2 -g $RG -n $AKSNAME --network-plugin none
+az aks create -k 1.25 -g $RG -n $AKSNAME --network-plugin none
 az aks get-credentials -g $RG -n $AKSNAME
-
-az vm create -n $VMNAME -g $RG \
-    --size $VMSIZE \
-    --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest \
-    --nsg $NSGNAME --nsg-rule NONE \
-    --vnet-name $VNETNAME --subnet $SUBNETNAME \
-    --ssh-key-values ~/.ssh/id_rsa.pub
