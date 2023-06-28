aacec0c8a7362bae90273417e53655a9125b8b2f
Ping He <tdihp@hotmail.com>
2023-06-28 15:39:32 +0800

    mncni: add VMSIZE for provision.sh

--- mncni/provision.sh
+++ mncni/provision.sh
@@ -4,7 +4,8 @@ set -o pipefail
 RG=mncni
 REGION=southeastasia
 AKSNAME=mncni
+VMSIZE=Standard_D2s_v4
 
 az group create -n $RG -l $REGION
-az aks create -k 1.25 -g $RG -n $AKSNAME --network-plugin none
+az aks create -k 1.25 -g $RG -n $AKSNAME -s $VMSIZE --network-plugin none
 az aks get-credentials -g $RG -n $AKSNAME
